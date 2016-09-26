51c372fc343b70b7501f3364dd5b6c7e86577dc4
Neths <vacher.michel@gmail.com>
2016-09-26 16:58:00 +0200

    Simple Array Sum

--- HackerRank/Algorithms/Warmup/Simple_Array_Sum.cs
+++ HackerRank/Algorithms/Warmup/Simple_Array_Sum.cs
@@ -1,12 +1,43 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using NUnit.Framework;
 
-namespace HackerRank.Algorithms.Warmup
+public class Simple_Array_Sum 
 {
-    class Simple_Array_Sum
+    private TextReader _in;
+    private TextWriter _out;
+
+    public Simple_Array_Sum(TextReader IN, TextWriter OUT)
     {
+        _out = OUT;
+        _in = IN;
+    }
+
+    public void Resolve()
+    {
+        var nb = int.Parse(_in.ReadLine());
+
+        _out.WriteLine(_in.ReadLine()
+            .Split(' ')
+            .Select(int.Parse)
+            .Sum()
+            .ToString());
+    }
+}
+
+[TestFixture]
+public class Test
+{
+    [Test]
+    public void GetSimpleSumArray()
+    {
+        var input = @"6
+1 2 3 4 10 11";
+
+        var output = new StringWriter();
+
+        new Simple_Array_Sum(new StringReader(input), output).Resolve();
+
+        Assert.AreEqual("31\r\n",output.GetStringBuilder().ToString());
     }
 }
---
+++ HackerRank/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRank
+{
+    public static class Program
+    {
+        public static void Main(String[] args)
+        {
+            var IN = Console.In;
+            var OUT = Console.Out;
+
+            int n = Convert.ToInt32(Console.ReadLine());
+            string[] arr_temp = Console.ReadLine().Split(' ');
+            int[] arr = Array.ConvertAll(arr_temp, Int32.Parse);
+        }
+    }
+}
