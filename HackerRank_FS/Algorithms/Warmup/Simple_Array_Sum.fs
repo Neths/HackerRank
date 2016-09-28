9c5e66c6da9ce6b82e51433cd0c03edc6c3afed3
Neths <vacher.michel@gmail.com>
2016-09-28 11:39:12 +0200

    add F# project

---
+++ HackerRank_FS/Algorithms/Warmup/Simple_Array_Sum.fs
@@ -0,0 +1,15 @@
+﻿namespace HackerRank_FS.Algorithms.Warmup
+
+open NUnit.Framework
+open FsUnit
+
+type Class1() = 
+    member this.X = "F#"
+
+
+[<TestFixture>]
+type TestClass =
+ 
+ [<Test>]
+ let ``When ``() =
+    Assert.AreEqual(1,1)
