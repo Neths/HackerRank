7df44b2afc42098c8769fa2626bc8756df134c66
Neths <vacher.michel@gmail.com>
2016-09-28 15:44:28 +0200

    Simple array sum F#

---
+++ HackerRank_FS/ReadMe.txt
@@ -0,0 +1,7 @@
+﻿Example of entrypoint
+
+[<EntryPoint>]
+let main argv = 
+    let solution = new SimpleArraySum(Console.In, Console.Out)
+    solution.Sum
+    0
--- HackerRank_FS/Simple_Array_Sum.fs
+++ HackerRank_FS/Simple_Array_Sum.fs
@@ -1,15 +1,29 @@
 ﻿namespace HackerRank_FS.Algorithms.Warmup
+//Sample Input
+//6
+//1 2 3 4 10 11
+
+//Sample Output
+//31
 
 open NUnit.Framework
 open FsUnit
+open System
+open System.IO
 
-type Class1() = 
-    member this.X = "F#"
+type SimpleArraySum(IN:TextReader, OUT:TextWriter) = 
+    member this.Sum = 
+        let nb = IN.ReadLine() |> int
+        OUT.Write (IN.ReadLine().Split [|' '|] |> Seq.map int |> Seq.sum)
 
 
 [<TestFixture>]
-type TestClass =
- 
- [<Test>]
- let ``When ``() =
-    Assert.AreEqual(1,1)
+type `` Given a simple array sum ``() =
+    let out = new StringWriter()
+
+    [<Test>]
+    member x.``When try sample use case ``() =
+        let simpleArraySum = new SimpleArraySum(new StringReader(@"6
+1 2 3 4 10 11"),out)
+        simpleArraySum.Sum
+        Assert.AreEqual("31",out.GetStringBuilder().ToString())
